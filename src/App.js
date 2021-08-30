15ada1f4a88da2616f40652783aa69f77706f824
Josefina <jstaudenmaier@udesa.edu.ar>
2021-08-30 09:08:25 -0300

    agrego React.Fragment en app.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import Header from '../src/components/header/Header';
 import Tarjeta from '../src/components/tarjeta/Tarjeta';
 import Footer from '../src/components/footer/Footer';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
       <Header/>
       <Tarjeta/>
       <Footer/>
-    </div>
+      </React.Fragment>
   );
 }
 
