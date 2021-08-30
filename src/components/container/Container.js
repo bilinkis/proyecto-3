bfa426d5f09bdfa19b31542b233712f9d2feb0e6
Josefina <jstaudenmaier@udesa.edu.ar>
2021-08-30 10:25:25 -0300

    cambios en proceso

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import Tarjeta from '../tarjeta/Tarjeta';
+
 
 class Container extends Component {
 
@@ -31,26 +33,10 @@ class Container extends Component {
         
         return (
             <React.Fragment>
-              <article>
-                <section class="navigation">
-                    <div>
-                        <i class="fas fa-chevron-left"></i>
-                        <i class="fas fa-chevron-right"></i>
-                    </div>
-                    <i class="far fa-window-close"></i>
-                </section>
-                <main>
-                    <img src="./img/image-default.png" alt=""/>
-                    <h3>Título/ Nombre</h3>
-                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint cumque velit minus facere laboriosam voluptatem impedit ea unde labore optio eius quis, dignissimos expedita. Culpa, soluta perspiciatis! Sint, laboriosam cum.</p>
-                    <section class="aditional-info">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse qui atque.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse qui atque.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse qui atque.</p>
-                    </section>
-                    <a href="">Ver más</a>
-                </main>
-            </article>
+              {this.state.movies.map ( (movies, index) =>{
+                  return <Tarjeta key = {index} title= {movies.title}></Tarjeta> 
+              })}
+
             </React.Fragment>
         )
 }
