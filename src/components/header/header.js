7d760d4b3eceaace13ab29320eddc0ecdfb84fc5
Josefina <jstaudenmaier@udesa.edu.ar>
2021-08-30 08:51:06 -0300

    componente header

---
+++ src/components/header/header.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+
+    constructor(props){ 
+    super(props)
+
+   /* this.state = {
+        valor: this.props.valorInicial
+    }*/
+}
+    render() {
+        
+        return (
+            <React.Fragment>
+              <header>
+        <h1>Título/ Nombre de la app</h1>
+        <section>
+            <p>Ordenar ASC/ DESC</p>
+            <i class="fas fa-th"></i>
+            <i class="fas fa-align-justify"></i>
+            <form action="">
+                <input type="text" name="search" id="" placeholder="Search"/>
+                <button type="submit"><i class="fas fa-search"></i></button>
+            </form>
+        </section>
+    </header>
+            </React.Fragment>
+        )
+    }
+
+
+
+}
