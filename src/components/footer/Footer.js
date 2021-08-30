5a60920e922562a1e4cc5e681003ebb883bcfcd2
Josefina <jstaudenmaier@udesa.edu.ar>
2021-08-30 09:01:55 -0300

    component Footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Footer {
+
+    render() {
+        
+        return (
+            <React.Fragment>
+             <footer>
+        <ul class="team">
+            <li>Josefina Staudenmaier</li>
+            <li>Nicolas Bilinkis</li>
+        </ul>
+    </footer>
+            </React.Fragment>
+        )
+    }
+
+
+
+}
+
+export default Footer;
