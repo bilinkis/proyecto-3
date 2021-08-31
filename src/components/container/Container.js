b494dbffde611cfd551c40fb6d4af68a978aaf20
Nico Bilinkis <nico@bilinkis.com>
2021-08-30 21:49:05 -0300

    tarjetas con fetch funcionando (sin css)

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -7,32 +7,31 @@ class Container extends Component {
     constructor(props){ 
     super(props);
     this.state ={
-        movies: []
+        movies: [],
     } 
     }
-
-    componentDidMount(){
+    fetchData = () => {
         fetch('https://api.themoviedb.org/3/trending/all/day?api_key=0ef54cf87594d6b6ca72ab2de24ffdc0')
 
-        .then(function(response){
+        .then((response)=>{
             return response.json();
         })        
-        .then(function(data){
-            console.log(data);
-            this.setState({
-                movies: data.results           
-            })
+        .then((data)=>{
+            
+            this.setState({movies:data.results})
         })
-        .catch(function(error){
+        .catch((error)=>{
             return console.log(error);
         })
+    }
+    componentDidMount(){
+        this.fetchData();
 }
 
 
-    render(){
-        
+    render = () => {
         return (
-              this.state.movies.map ( (movies, index) =>{
+              this.state.movies.map ( (movies, index) => {
                   return <Tarjeta key = {index} title= {movies.title} rating = {movies.vote_average} descripcion= {movies.overview}/> 
               })
 
