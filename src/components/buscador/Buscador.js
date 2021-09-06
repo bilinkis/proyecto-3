b43fffc69d77384cc75271f57dda3c7449b1ee5d
Josefina <jstaudenmaier@udesa.edu.ar>
2021-09-06 09:40:03 -0300

    componente buscador

---
+++ src/components/buscador/Buscador.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+export default class Buscador extends Component{
+    constructor(){
+        super();
+        //Valor inicial del estado
+        this.state ={
+            valor: ""
+
+        }
+    }
+
+    prevenirSubmit(event){
+        evento.preventDefault();
+    }
+
+    capturarInfo(event){
+    console.log(event.target.value)
+    this.setState({
+    valor: event.target.value
+})
+    }
+
+    render(){
+        return(
+            <form onSubmit={(event)=> this.prevenirSubmit(event)}>
+                <input onChange={(event)=> this.capturarInfo(event)}type="text"/>
+                <input type="submit" />
+            </form>
+        )
+    }
+}
